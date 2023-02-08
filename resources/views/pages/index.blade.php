a038c51926a84894d273222c995717db341f8b42
jonathan010603 <jonathan010603@gmail.com>
2023-02-08 15:32:20 -0300

    build (Initial commit)!: basic homepage structure
    - Created the main homepage divs
    - Added the main measures and colors

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,18 @@
+<x-bootstrap>
+    <main class="homeContainer-main">
+        <div class="topBox-div d-flex align-items-center justify-content-between">
+            <span><b>Colaboradores de Agape solucöes</b></span>
+            <button>Adicionar</button>
+        </div>
+
+        <div class="people-div">
+            <div class="person-div">
+                
+            </div>
+
+            <div class="person-div">
+
+            </div>
+        </div>
+    </main>
+</x-bootstrap>
